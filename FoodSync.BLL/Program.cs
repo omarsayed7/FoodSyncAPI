c81dccaf2f9831c7691d41d7d7e38032ef816b11
omarsayed7 <sayedomar74@gmail.com>
2022-02-23 17:30:14 +0200

    Add project files.

---
+++ FoodSync.BLL/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FoodSync.BLL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
