489f8d017bef16cc41247406563da4e58c3a3578
omarsayed7 <sayedomar74@gmail.com>
2022-05-29 15:46:04 +0200

    test

---
+++ FoodSync.API/Controllers/DailyOperaionsController.cs
@@ -0,0 +1,23 @@
+﻿using FoodSync.BLL.Abstract;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FoodSync.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DailyOperaionsController : ControllerBase
+    {
+        private readonly IDailyOperaions _business;
+
+        public DailyOperaionsController(IDailyOperaions dailyOperation)
+        {
+            _business = dailyOperation;
+        }
+
+    }
+}
---
+++ FoodSync.BLL/Concrete/DailyOperations.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FoodSync.BLL.Abstract;
+
+namespace FoodSync.BLL.Concrete
+{
+    public class DailyOperations: IDailyOperaions
+    {
+    }
+}
